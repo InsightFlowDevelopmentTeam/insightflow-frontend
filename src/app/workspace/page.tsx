6bdf96576165bb925e882896bae830685913d895
Alberto Lyons <alberto.lyons@alumnos.ucn.cl>
2025-11-25 20:29:59 -0300

    chore: changed .env files

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -2,12 +2,14 @@ import { GetWorkspaceByUser } from '@/src/models/GetWorkspaceByUser';
 import Board from '@/src/components/Board';
 import Link from 'next/link';
 
-const WORKSPACE_URL = process.env.workspace_url;
+const WORKSPACE_URL = process.env.WORKSPACES_URL;
 
 const workspacesCache: { data: GetWorkspaceByUser[] | null } = { data: null };
 
 async function getWorkspaces(): Promise<GetWorkspaceByUser[]> {
+  // Temporalmente se usa un userId hardcodeado
   const userId: string = "b3850a65-61d9-4417-8b03-de3a700d7064";
+  // Se tiene que reemplazar por obtener el userId del usuario logueado
   const response = await fetch(WORKSPACE_URL + 'workspaces?userId=' + userId);
   const workspaces: GetWorkspaceByUser[] = await response.json();
   for (const workspace of workspaces) {
